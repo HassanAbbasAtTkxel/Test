bbb2703464081635b169430c53aa0fabf25d5b7d
Hassan Abbas <hassan.abbas@tkxel.io>
2022-09-12 18:15:23 +0500

    Jhon.js added

---
+++ jhon.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class jhon extends Component {
+  render() {
+    return (
+      <View>
+        <Text>jhon</Text>
+      </View>
+    )
+  }
+}
