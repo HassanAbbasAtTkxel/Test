f3646acc42c7ee5af7b906f44137403f9432ee6f
Hassan Abbas <hassan.abbas@tkxel.io>
2022-09-12 18:14:11 +0500

    wow file added

---
+++ wow.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class wow extends Component {
+  render() {
+    return (
+      <View>
+        <Text>wow</Text>
+      </View>
+    )
+  }
+}
